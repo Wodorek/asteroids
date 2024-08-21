38d525337b9524481166ad333a27c0572f86a5e8
Wodorek <krol.w94@gmail.com>
2024-08-21 18:32:40 +0200

    added player movement

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ def main():
 
         screen.fill((0, 0, 0))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        passed_time = clock.tick(60) / 1000
-        dt += passed_time
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == '__main__':
