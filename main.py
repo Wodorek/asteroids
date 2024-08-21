1adaf4220c125bcc589e7f79a7cc1f67c0a6749e
Wodorek <krol.w94@gmail.com>
2024-08-21 18:57:10 +0200

    grouped entities

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     clock = pygame.time.Clock()
     dt = 0
 
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -18,8 +22,13 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+
+        for d in drawable:
+            d.draw(screen)
+
+        for u in updatable:
+            u.update(dt)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
