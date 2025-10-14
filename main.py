3b13b2541b5baf630e1b2cfa17cc3df212d130bf
Wodorek <krol.w94@gmail.com>
2025-10-14 15:18:37 +0200

    ch2/l3

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ def main():
 Screen height: {SCREEN_HEIGHT}""")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        tick = clock.tick(60)
+        dt = tick
 
 
 if __name__ == "__main__":
